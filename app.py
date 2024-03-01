f6956a70fb739f19763aa138191408c4b9405404
Antony <antony@pop-os.localdomain>
2024-03-01 21:07:08 +0300

    Modify model path

--- app.py
+++ app.py
@@ -23,11 +23,8 @@ app = Flask(__name__)
 # logging.info("Model loaded successfully.")
 
 def load_model():
-    # Construct the absolute path to the model file
     model_path = os.path.join(os.getcwd(), 'models', 'XGBClassifier_model.joblib')
-
     try:
-        # Load the model
         model = joblib.load(model_path)
         logging.info("Model loaded successfully.")
         return model
@@ -35,19 +32,8 @@ def load_model():
         logging.error(f"Error loading the model: {str(e)}")
         return None
 
-if __name__ == "__main__":
-    # Configure logging to output to the console
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
-    # Load the model
-    model = load_model()
-
-    if model is not None:
-        # Continue with your application logic here
-        pass
-    else:
-        # Handle the case when the model loading fails
-        logging.error("Application cannot proceed without a loaded model.")
+# Load the model
+model = load_model()
 
 # Mapping dictionary
 #type_mapping = {'benign': 0, 'defacement': 1, 'malware': 2, 'phishing': 3}
